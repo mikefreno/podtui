bfea6816ef3615018b073d7ddda31501cc772800
Mike Freno <michael@freno.me>
2026-02-06 15:02:21 -0500

    dead

--- src/utils/waveform.ts
+++
@@ -1,8 +0,0 @@
-export const createWaveform = (width: number): number[] => {
-  const data: number[] = []
-  for (let i = 0; i < width; i += 1) {
-    const value = 0.2 + Math.abs(Math.sin(i / 3)) * 0.8
-    data.push(Number(value.toFixed(2)))
-  }
-  return data
-}
