8ac1ec116224aee12b0e1233ad91ae51d7908cb5
Michael Freno <michaelt.freno@gmail.com>
2026-08-10 13:45:24 -0400

    bump VERSION to 0.3.1

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import type { Feed } from "./types/feed"
 import type { Episode } from "./types/episode"
 
-const VERSION = "0.3.0";
+const VERSION = "0.3.1";
 
 interface CliArgs {
 	version: boolean;
